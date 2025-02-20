91723480bb873068dd9cab55fb09f3c5f80d8c74
BulatovaTati <kohodatat@gmail.com>
2025-02-20 20:33:59 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import { addOneContact } from './scripts/addOneContact.js';
-import { countContacts } from './scripts/countContacts.js';
-import { generateContacts } from './scripts/generateContacts.js';
-import { getAllContacts } from './scripts/getAllContacts.js';
-import { removeAllContacts } from './scripts/removeAllContacts.js';
-import { removeLastContact } from './scripts/removeLastContact.js';
+ 
