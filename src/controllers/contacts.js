dc38f02c16e67a17a1fdfdf424533bf0561d820e
BulatovaTati <kohodatat@gmail.com>
2025-03-02 16:38:02 +0200

    add crud

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,5 +1,11 @@
 import createHttpError from 'http-errors';
-import { getContactById, getContacts } from '../services/contacts.js';
+import {
+  createContact,
+  deleteContact,
+  getContactById,
+  getContacts,
+  updateContact,
+} from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
   const contacts = await getContacts();
@@ -15,7 +21,7 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.json({
@@ -23,4 +29,42 @@ export const getContactByIdController = async (req, res, next) => {
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.json({
+    status: 201,
+    message: `Successfully created a contact!`,
+    data: contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const student = await deleteContact(contactId);
+
+  if (!student) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(204).send();
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,32 @@ export const getContacts = async () => {
 export const getContactById = async (contactId) => {
   return await ContactsCollection.findById(contactId);
 };
+
+export const createContact = async (payload) => {
+  return await ContactsCollection.create(payload);
+};
+
+export const deleteContact = async (contactId) => {
+  return await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+};
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    student: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
