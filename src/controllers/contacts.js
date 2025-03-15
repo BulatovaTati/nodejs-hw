897005b56e4c91ad1b3491d9c49991ea4bf4d63a
BulatovaTati <kohodatat@gmail.com>
2025-03-15 19:28:09 +0200

    add cloudinary load

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -11,6 +11,8 @@ import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
@@ -69,9 +71,12 @@ export const patchContactController = async (req, res) => {
   let photoUrl;
 
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
-
   const updatedContact = await updateContact(
     contactId,
     { ...req.body, photo: photoUrl },
--- src/services/auth.js
+++ src/services/auth.js
@@ -127,7 +127,9 @@ export const requestResetToken = async (email) => {
 
     const html = template({
       name: user.name,
-      link: `${getEnvVar('APP_DOMAIN')}/reset-password?token=${resetToken}`,
+      link: `${getEnvVar(
+        'APP_DOMAIN',
+      )}/auth/reset-password?token=${resetToken}`,
     });
 
     await sendEmail({
