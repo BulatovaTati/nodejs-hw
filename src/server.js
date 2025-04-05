94b67b82bcbe75f438e10934b6e60178d33e89a0
BulatovaTati <kohodatat@gmail.com>
2025-04-05 11:51:02 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,11 +17,11 @@ export const setupServer = async () => {
   const app = express();
 
   app.use(express.json());
-  app.use(cors(corsOptions));
   const corsOptions = {
     origin: 'http://localhost:5173',
     credentials: true,
   };
+  app.use(cors(corsOptions));
   app.use(cookieParser());
 
   app.use(
