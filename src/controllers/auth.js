78da78235bd1593c6c3c1aedefbdcc926d5b3066
Tati <102695106+BulatovaTati@users.noreply.github.com>
2025-04-07 20:00:42 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,11 +15,15 @@ const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'none', 
+    secure: true
   });
 
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     expires: new Date(Date.now() + ONE_DAY),
+    sameSite: 'none', 
+    secure: true
   });
 };
 
